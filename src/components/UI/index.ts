e6b63da061db645798e930b3eac576fd938013ed
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-27 11:56:55 +0300

    feat: add exports of developed components

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,1 +1,5 @@
 export { default as FilterButton } from './FilterButton';
+export { default as Image } from './Image';
+export { default as SearchButton } from './SearchButton';
+export { default as SearchInput } from './SearchInput';
+export { default as ShowMoreButton } from './ShowMoreButton';
