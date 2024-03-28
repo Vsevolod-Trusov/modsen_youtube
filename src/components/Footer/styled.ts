10443925f5a4fe9b0536e2fc39a4c49f23b335de
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 18:26:37 +0300

    fix: change styles of footer section

--- src/components/Footer/styled.ts
+++ src/components/Footer/styled.ts
@@ -1,9 +1,10 @@
 import { styled } from 'styled-components';
 
-const StyledFooterContainer = styled('div')({
-  height: '100px',
-  backgroundColor: 'blue',
+const StyledFooterContainer = styled('div')(({ theme }) => ({
+  height: '299px',
   flexShrink: '0',
-});
+  padding: '65px 10px 10px 53px',
+  borderTop: `2px solid ${theme.colors.gray}`,
+}));
 
 export { StyledFooterContainer };
