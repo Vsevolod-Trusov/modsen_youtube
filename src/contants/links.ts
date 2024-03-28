90b7c5fd2aa0bad022471e1e867ab67ccbc0d0f6
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 18:29:02 +0300

    feat: add links

---
+++ src/contants/links.ts
@@ -0,0 +1,8 @@
+const LINKS = {
+  LINKEDIN: 'https://www.linkedin.com/',
+  INSTAGRAM: 'https://www.instagram.com',
+  TWITTER: 'https://twitter.com/',
+  FACEBOOK: 'https://www.facebook.com/',
+};
+
+export { LINKS };
