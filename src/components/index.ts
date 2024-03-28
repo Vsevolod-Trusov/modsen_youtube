16b15bd9f09c1d1592bda65eac4fc32c7af26464
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 15:41:50 +0300

    feat: add exports for search and categories sections

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
+export { default as CategoriesSection } from './CategoriesSection';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Logo } from './Logo';
 export { default as Main } from './Main';
 export { default as SearchBar } from './SearchBar';
+export { default as SearchSection } from './SearchSection';
 export * from './UI';
