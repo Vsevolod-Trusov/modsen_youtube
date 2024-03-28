cf89da8f145505747992c81f74d6e03f853772f9
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 15:39:19 +0300

    feat: develop search section

---
+++ src/components/SearchSection/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Logo, SearchBar, ToggleButton } from '@/components';
+import { DICTIONARY } from '@/contants';
+
+import { StyledContainer } from './styled';
+
+const SearchSection = () => {
+  return (
+    <StyledContainer>
+      <div>
+        <Logo title={DICTIONARY.LOGO_TITLE} />
+      </div>
+      <SearchBar placeholder={DICTIONARY.SEARCH_INPUT_PLACEHOLDER} />
+      <div>
+        <ToggleButton />
+      </div>
+    </StyledContainer>
+  );
+};
+
+export default SearchSection;
---
+++ src/components/SearchSection/styled.ts
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+const StyledContainer = styled('div')({
+  display: 'flex',
+  width: '100%',
+  justifyContent: 'center',
+  flex: '0.5',
+  padding: '0 5px',
+});
+
+export { StyledContainer };
