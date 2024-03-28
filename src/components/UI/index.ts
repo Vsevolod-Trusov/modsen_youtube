df642ea12f5fafc808192da02ec15e204ea83eed
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 18:28:03 +0300

    fix: add exports

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,6 +1,7 @@
 export { default as FilterButton } from './FilterButton';
+export { default as IconButton } from './IconButton';
 export { default as Image } from './Image';
+export { default as ShowMoreButton } from './ScrollButton';
 export { default as SearchButton } from './SearchButton';
 export { default as SearchInput } from './SearchInput';
-export { default as ShowMoreButton } from './ShowMoreButton';
 export { default as ToggleButton } from './ToggleButton';
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,8 +1,11 @@
 export { default as CategoriesSection } from './CategoriesSection';
+export { default as CompanySection } from './CompanySection';
+export { default as FilmCard } from './FilmCard';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Logo } from './Logo';
 export { default as Main } from './Main';
+export { default as MetadataSection } from './MetadataSection';
 export { default as SearchBar } from './SearchBar';
 export { default as SearchSection } from './SearchSection';
 export * from './UI';
