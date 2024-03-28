a3a51636d4b1ddab1b195363b6cd986a93447ca3
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 18:25:30 +0300

    feat: add social networks images

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,8 @@
+export { default as AvatarIcon } from './AvatarIcon.png';
+export { default as Facebook } from './Facebook.png';
+export { default as Instagram } from './Instagram.png';
+export { default as Linkedin } from './Linkedin.png';
 export { default as LogoIcon } from './Logo.png';
+export { default as Preview } from './Preview.png';
 export { default as SearchIcon } from './Search.png';
+export { default as Twitter } from './Twitter.png';
