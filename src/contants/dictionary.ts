63c0eda1075169beec0d631a24497ebdd1c92c9f
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-28 18:28:51 +0300

    feat: add new ui text

--- src/contants/dictionary.ts
+++ src/contants/dictionary.ts
@@ -1,6 +1,14 @@
 const DICTIONARY = {
   SEARCH_INPUT_PLACEHOLDER: 'Search',
   LOGO_TITLE: 'ModsenFilms',
+  SHOW_MORE: 'Show More',
+  MODSEN_COMPANY: '2023 Modsen company',
+  POLICY: 'TermsPrivacyPolicy & Safety',
+  YOU_TUBE_WORKS: 'How YouTube works',
+  TEST_FEATURE: 'Test new features',
+  ABOUT_COPYRIGHT: 'About Press Copyright',
+  CONTACT_US: 'Contact us Creators',
+  ADVERTISE_DEVELOPERS: 'Advertise Developers',
 };
 
 export { DICTIONARY };
