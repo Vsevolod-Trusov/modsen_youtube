1df611ca38cf43ed89ba66108998fbc5beb095d1
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-29 16:11:13 +0300

    feat: add menu icon

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -3,6 +3,7 @@ export { default as Facebook } from './Facebook.png';
 export { default as Instagram } from './Instagram.png';
 export { default as Linkedin } from './Linkedin.png';
 export { default as LogoIcon } from './Logo.png';
+export { default as Menu } from './Menu.png';
 export { default as Preview } from './Preview.png';
 export { default as SearchIcon } from './Search.png';
 export { default as Twitter } from './Twitter.png';
