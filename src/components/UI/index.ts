16ef1616a62c7b04082eb1361351d92511033f48
vsevolod <vsevolod_trusov_00@mail.ru>
2024-03-29 16:13:47 +0300

    fix: add export menu button

--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,6 +1,7 @@
 export { default as FilterButton } from './FilterButton';
 export { default as IconButton } from './IconButton';
 export { default as Image } from './Image';
+export { default as MenuButton } from './MenuButton';
 export { default as ShowMoreButton } from './ScrollButton';
 export { default as SearchButton } from './SearchButton';
 export { default as SearchInput } from './SearchInput';
