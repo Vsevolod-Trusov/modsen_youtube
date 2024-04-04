eb9a08685871909a34e6ce0d657a4a7bb00b5fff
vsevolod <vsevolod_trusov_00@mail.ru>
2024-04-04 16:12:19 +0300

    fix: add chenge theme in redux store

--- src/components/UI/ToggleButton/index.tsx
+++ src/components/UI/ToggleButton/index.tsx
@@ -1,5 +1,10 @@
 import React, { FC } from 'react';
 
+import { useDispatch } from 'react-redux';
+
+import { useTypedSelector } from '@/hooks';
+import { getCurrentThemeValue, toggleTheme } from '@/store/slices';
+
 import {
   StyledInput,
   StyledLabel,
@@ -8,11 +13,18 @@ import {
 } from './styled';
 
 const ToggleButton: FC = () => {
+  const dispatch = useDispatch();
+  const currentTheme = useTypedSelector(getCurrentThemeValue);
+
+  const handleToggle = () => {
+    dispatch(toggleTheme(!currentTheme));
+  };
+
   return (
     <StyledToggleContainer>
       <StyledLabel>
-        <StyledInput type='checkbox' className='input' />
-        <StyledSpan className='slider'></StyledSpan>
+        <StyledInput type='checkbox' className='input' onClick={handleToggle} />
+        <StyledSpan className='slider' />
       </StyledLabel>
     </StyledToggleContainer>
   );
