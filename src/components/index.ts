5bb569c0bf3a53d21f4bcd8847fa08a81b307ce2
vsevolod <vsevolod_trusov_00@mail.ru>
2024-04-04 16:13:11 +0300

    fix: add imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,8 @@
 export { default as CategoriesSection } from './CategoriesSection';
 export { default as CompanySection } from './CompanySection';
+export { default as CustomThemeProvider } from './CustomThemeProvider';
 export { default as FilmCard } from './FilmCard';
+export { default as FilmsList } from './FilmsList';
 export { default as Footer } from './Footer';
 export { default as Header } from './Header';
 export { default as Logo } from './Logo';
