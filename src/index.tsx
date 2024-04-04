7dc423753149bae7ab66e479d77794a76629b10a
vsevolod <vsevolod_trusov_00@mail.ru>
2024-04-04 16:12:51 +0300

    refactor: change code

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
+import { Provider } from 'react-redux';
 
-import getGlobalStyles, { DEFAULT_THEME } from '@/assets';
+import getGlobalStyles from '@/assets';
+import { CustomThemeProvider } from '@/components';
 import { MoviesPage } from '@/pages';
+import { store } from '@/store';
 
 const container = document.getElementById('root') as HTMLElement;
 const root = createRoot(container);
@@ -12,9 +14,11 @@ const GlobalStyles = getGlobalStyles();
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={DEFAULT_THEME}>
-      <GlobalStyles />
-      <MoviesPage />
-    </ThemeProvider>
+    <Provider store={store}>
+      <CustomThemeProvider>
+        <GlobalStyles />
+        <MoviesPage />
+      </CustomThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
