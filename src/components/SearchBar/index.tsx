ec09839fadaa53bacc7923d893f6b07533abaac5
vsevolod <vsevolod_trusov_00@mail.ru>
2024-04-08 11:43:51 +0300

    fix: add controled input

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,15 +1,33 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
+import { useDispatch } from 'react-redux';
 
 import { SearchIcon } from '@/assets';
 import { Image, SearchButton, SearchInput } from '@/components';
+import { EMPTY_STRING } from '@/contants';
+import { setSearchValue } from '@/store/slices';
 
 import { StyledSearchImageWrapper, StyledSearchSection } from './styled';
 
 const SearchBar: FC<ISearchBar> = ({ placeholder }) => {
+  const dispatch = useDispatch();
+  const [searchValue, setValue] = useState<string>(EMPTY_STRING);
+
+  const handleSearchValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const handleSearch = () => {
+    dispatch(setSearchValue(searchValue));
+  };
+
   return (
     <StyledSearchSection>
-      <SearchInput placeholder={placeholder} />
-      <SearchButton>
+      <SearchInput
+        placeholder={placeholder}
+        value={searchValue}
+        onChange={handleSearchValue}
+      />
+      <SearchButton onClick={handleSearch}>
         <StyledSearchImageWrapper>
           <Image src={SearchIcon} />
         </StyledSearchImageWrapper>
