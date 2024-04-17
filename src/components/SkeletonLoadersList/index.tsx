5daf762df41731876a708186b3d2e5a014cbb2ca
vsevolod <vsevolod_trusov_00@mail.ru>
2024-04-17 11:29:19 +0300

    fix: magic numbers

--- src/components/SkeletonLoadersList/index.tsx
+++ src/components/SkeletonLoadersList/index.tsx
@@ -2,11 +2,12 @@ import React, { FC, useMemo } from 'react';
 
 import { SkeletonLoader } from '@/components';
 import { StyledFilmsContainer } from '@/components/FilmsList/styled';
+import { ZERO } from '@/contants';
 import { ISkeletonLoadersList } from '@/types';
 
 const SkeletonLoadersList: FC<ISkeletonLoadersList> = ({ amount }) => {
   const loaders = useMemo(() => {
-    return new Array(amount).fill(0);
+    return new Array(amount).fill(ZERO);
   }, [amount]);
 
   return (
